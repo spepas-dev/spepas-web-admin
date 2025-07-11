be04401cd9b7ca55fe47add0a70062f9b080dbb6
Spa Pas <jboadi@spepas.com>
2025-07-12 01:20:07 +0200

    apm fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,19 @@
-// main.tsx or index.tsx — must be at the very top
-import { init as initApm } from '@elastic/apm-rum';
-
-initApm({
-  serviceName: 'Spepas Web Admin',
-  serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
-  serviceVersion: '0.0.1',
-  active: true,
-  breakdownMetrics: true
-});
-
-// OPTIONAL: force it just in case
-// apm.startTransaction('Page Load', 'page-load');
-
 import './index.css';
 
+import { init } from '@elastic/apm-rum';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+
+init({
+  serviceName: 'Spepas Web Admin',
+  serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
+  serviceVersion: '0.0.1',
+  active: true
+  // breakdownMetrics: true,
+  // distributedTracingOrigins: ['*']
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
