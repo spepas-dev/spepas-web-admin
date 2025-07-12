37a60f10295bb69d7704918c4f7c7537a311130b
Spa Pas <jboadi@spepas.com>
2025-07-12 10:33:17 +0200

    apm-fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,38 @@
 import './index.css';
 
-import { init } from '@elastic/apm-rum';
+import { init as initApm } from '@elastic/apm-rum';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
-init({
-  serviceName: 'Spepas Web Admin',
-  serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
-  serviceVersion: '0.0.1',
-  active: true
-  // breakdownMetrics: true,
-  // distributedTracingOrigins: ['*']
+async function getClientIP() {
+  try {
+    const res = await fetch('https://api.ipify.org?format=json');
+    const data = await res.json();
+    return data.ip; // e.g., "203.0.113.42"
+  } catch {
+    return null;
+  }
+}
+
+getClientIP().then((ip) => {
+  const apm = initApm({
+    serviceName: 'Spepas Web Admin',
+    serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
+    serviceVersion: '0.0.1',
+    active: true,
+    breakdownMetrics: true,
+    distributedTracingOrigins: ['*']
+  });
+
+  if (ip) {
+    apm.setCustomContext({
+      client: {
+        ip
+      }
+    });
+  }
 });
 
 createRoot(document.getElementById('root')!).render(
