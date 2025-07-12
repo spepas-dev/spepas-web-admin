1899582a497bdfbfc1cc0b9d3c6a885e38f511cf
Spa Pas <jboadi@spepas.com>
2025-07-12 18:07:57 +0200

    apm-fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,41 +6,41 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
-// async function getClientIP() {
-//   try {
-//     const res = await fetch('https://api.ipify.org?format=json');
-//     console.log('Response From Client=================================, ', res);
-//     const data = await res.json();
-//     return data.ip; // e.g., "203.0.113.42"
-//   } catch {
-//     return null;
-//   }
-// }
+async function getClientIP() {
+  try {
+    const res = await fetch('https://api.ipify.org?format=json');
+    console.log('Response From Client=================================, ', res);
+    const data = await res.json();
+    return data.ip; // e.g., "203.0.113.42"
+  } catch {
+    return null;
+  }
+}
 
-// getClientIP().then((ip) => {
-//   console.log('Client Side IP Address=============================, ', ip);
-//   const apm = initApm({
-//     serviceName: 'Spepas Web Admin',
-//     serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
-//     serviceVersion: '0.0.1',
-//     active: true
-//   });
+getClientIP().then((ip) => {
+  console.log('Client Side IP Address=============================, ', ip);
+  const apm = initApm({
+    serviceName: 'Spepas Web Admin',
+    serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
+    serviceVersion: '0.0.1',
+    active: true
+  });
 
-//   if (ip) {
-//     apm.setCustomContext({
-//       client: {
-//         ip
-//       }
-//     });
-//   }
-// });
-
-initApm({
-  serviceName: 'Spepas Web Admin',
-  serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
-  serviceVersion: '0.0.1',
-  active: true
+  if (ip) {
+    apm.setCustomContext({
+      client: {
+        ip
+      }
+    });
+  }
 });
+
+// initApm({
+//   serviceName: 'Spepas Web Admin',
+//   serverUrl: import.meta.env.VITE_ELASTIC_APM_SERVER,
+//   serviceVersion: '0.0.1',
+//   active: true
+// });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
